4b69f8ea017407c6f9bfa3afb36beebdaaace1d2
Rev8617 <victormasson21@gmail.com>
2019-04-16 18:30:54 +0200

    Version 1 - Adding elements
    - Adding html elements
    - Include holder images folder
    - CSS positioning still off

---
+++ slider_script.js
@@ -0,0 +1,8 @@
+var someElement = document.querySelector('someElement'); // element like 'form' 'div' 'p' ...
+var someID = document.getElementById('someID'); // ID you set yourself with ID = //
+
+
+
+var button = document.querySelector('button');
+var left = document.getElementById('left');
+var right = document.getElementById('right');
