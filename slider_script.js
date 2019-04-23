5ceab8e668f29a79675c848c5f774253bc1d381d
Rev8617 <victormasson21@gmail.com>
2019-04-23 17:33:17 +0200

    Version 3 - Create image mosaic and slide effect
    - Adding for Loop to create a mosaïc with all the images to display.
    - Adding 'slideRight' function adjusting the left margin of the image container to display various images.
    - Linking 'slideRight' function to existing buttons using event listeners.

--- slider_script.js
+++ slider_script.js
@@ -1,15 +1,26 @@
 var currentImage = document.getElementById('currentImage');
-
+var imgContainer = document.getElementById('imgContainer');
 var prev = document.getElementById('prev');
 var next = document.getElementById('next');
 
-
-var i=1;
-function swapImage() {
-  i++
-  var newSrc = 'imgfolder/img' + i + '.png'
-  currentImage.setAttribute('src', newSrc);
-  if (i==5) {i=0}
+/* Creates a mosaic with all the images to display */
+for (let i = 2; i <= 5; i++) {
+  var newImage = currentImage.cloneNode(false);
+  imgContainer.appendChild(newImage);
+  var newSrc = 'imgfolder/img' + i + '.png';
+  newImage.setAttribute('src', newSrc);
+  newImage.setAttribute('style', 'margin-left:' + (i - 1) * 600 + 'px');
 }
-prev.addEventListener('click', swapImage);
-next.addEventListener('click', swapImage);
+
+/* Adjusts the left margin of the image container to display various images */
+var j = 1;
+var newMargin = 0;
+function slideRight() {
+  newMargin = -(600 * j);
+  imgContainer.setAttribute('style', 'margin-left:' + newMargin + 'px');
+  j += 1;
+  if (j == 5) {j = 0}
+}
+
+prev.addEventListener('click', slideRight);
+next.addEventListener('click', slideRight);
