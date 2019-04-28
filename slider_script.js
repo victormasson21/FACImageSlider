8a3c2c148e99843154b7770df08cb70f7a4cd0c9
Rev8617 <victormasson21@gmail.com>
2019-04-28 12:34:33 +0200

    Fix slideRight function bug
    Removed temporary print() function (used to debug in the console) which caused a bug in the slideRight function.

--- slider_script.js
+++ slider_script.js
@@ -26,17 +26,19 @@ function slideLeft() {
   j -= 1;
   if (j < 0) {j = 4}
   newMargin = -(sliderContainer.offsetWidth * j);
-  currentImage.setAttribute('style', 'margin-left:' + newMargin + 'px');
-  print()
+  currentImage.setAttribute('style', 'margin-left: ' + newMargin + 'px');
 }
 
+/* Link keyup events to slideLeft/slideRight functions */
 window.addEventListener('keyup', function(e) {
   if (e.key == 'ArrowRight') {slideRight()}
   if (e.key == 'ArrowLeft') {slideLeft()}
 })
 
+/* Link previous/next buttons to slideLeft/slideRight functions */
 prev.addEventListener('click', slideLeft);
 next.addEventListener('click', slideRight);
+
 
 /* Adjusts margin when resizing */
 window.addEventListener('resize', function() {
@@ -44,10 +46,10 @@ window.addEventListener('resize', function() {
   currentImage.setAttribute('style', 'margin-left:' + newMargin + 'px');
 })
 
+
 /* Autoplay */
 
 var t = 1;
-
 function autoplay() {
   t++;
   if (t % 2 === 0) {
