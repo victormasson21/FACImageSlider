09c801a523ec4d44f89927439a00e4d2c11715d7
Rev8617 <victormasson21@gmail.com>
2019-04-28 12:10:24 +0200

    Adding autoplay function
    Add function autoplay():
    - var t = counter that goes up by 1 after each click
    - turned into on/off switch with if/else statement checking the %2 remainder
    - using setInterval to autoplay on 'on'
    - adjustement to button styling to reflect play'pause behavior
    - hides the previous/next buttons when autoplaying (nice UX but heavy code)
    
    Other changes:
    - Add var 'sliderContainer' (used to pull slider's offsetWidth)
    - Fix slideRight functions (swith imgContainer for currentImage
    - Fix 'prev' click event listener (now triggers slideLeft function)

--- slider_script.js
+++ slider_script.js
@@ -1,5 +1,6 @@
 var currentImage = document.getElementById('currentImage');
 var imgContainer = document.getElementById('imgContainer');
+var sliderContainer = document.getElementsByClassName('sliderContainer')[0];
 var prev = document.getElementById('prev');
 var next = document.getElementById('next');
 
@@ -18,7 +19,7 @@ function slideRight() {
   j += 1;
   if (j == 5) {j = 0}
   newMargin = -(sliderContainer.offsetWidth * j);
-  imgContainer.setAttribute('style', 'margin-left:' + newMargin + 'px');
+  currentImage.setAttribute('style', 'margin-left:' + newMargin + 'px');
 }
 
 function slideLeft() {
@@ -34,7 +35,7 @@ window.addEventListener('keyup', function(e) {
   if (e.key == 'ArrowLeft') {slideLeft()}
 })
 
-prev.addEventListener('click', slideRight);
+prev.addEventListener('click', slideLeft);
 next.addEventListener('click', slideRight);
 
 /* Adjusts margin when resizing */
@@ -42,3 +43,33 @@ window.addEventListener('resize', function() {
   newMargin = -(sliderContainer.offsetWidth * j);
   currentImage.setAttribute('style', 'margin-left:' + newMargin + 'px');
 })
+
+/* Autoplay */
+
+var t = 1;
+
+function autoplay() {
+  t++;
+  if (t % 2 === 0) {
+    ongoing = window.setInterval(slideRight, 1000);
+    buttonOn.innerHTML = `&#9724;`;
+    var newStyle = 'font-size: 1.3em; padding-left: 5px;';
+    buttonOn.setAttribute('style', newStyle);
+
+    var hideBtns = 'visibility: hidden';  // not necessary?
+    prev.setAttribute('style', hideBtns);  // not necessary?
+    next.setAttribute('style', hideBtns);  // not necessary?
+  }
+  else {
+    window.clearInterval(ongoing);
+    buttonOn.innerHTML = `&#9658;`;
+    var newStyle = 'font-size: 1.2em; padding-left: 10px;';
+    buttonOn.setAttribute('style', newStyle);
+
+    var hideBtns = 'visibility: visible';  // not necessary?
+    prev.setAttribute('style', hideBtns);  // not necessary?
+    next.setAttribute('style', hideBtns);  // not necessary?
+  }
+}
+
+buttonOn.addEventListener('click', autoplay);
